6d2c84a1b796a18aac9da3e5e1d2bad499af0dab
Reuven Lerner <reuven@lerner.co.il>
2022-05-29 10:25:32 +0300

    gitautosync update 69

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,4 +1,6 @@
 # this file, __init__.py, will be run automatically when someone
 # imports mypackage
 
+import b
+import a
 print('Hello from mypackage!')
--- mypackage/flycheck___init__.py
+++
@@ -1,6 +0,0 @@
-# this file, __init__.py, will be run automatically when someone
-# imports mypackage
-
-print('Hello from mypackage!')
-import a
-import b
