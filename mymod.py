aaf404c2bb7582f86582db6dd38a84762a73e255
Reuven Lerner <reuven@lerner.co.il>
2022-05-29 09:51:10 +0300

    gitautosync update 34

--- mymod.py
+++ mymod.py
@@ -1,7 +1,7 @@
 # __name__ is a string, the name of the current file
 
-id __name__ == '__main__':
-print(f'Hello from {__name__}!')
+if __name__ == '__main__':
+    print(f'Hello from {__name__}!')
 
 x = 100
 
@@ -12,5 +12,5 @@ def hello(name):
     return f'Hello, {name}!'
 
 
-id __name__ == '__main__':
+if __name__ == '__main__':
     print(f'Goodbye from {__name__}!')
