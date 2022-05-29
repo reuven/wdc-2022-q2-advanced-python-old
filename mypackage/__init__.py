a06602725fe48d3d547ea1650b9ddb074fc64cf8
Reuven Lerner <reuven@lerner.co.il>
2022-05-29 10:26:48 +0300

    gitautosync update 72

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,7 +1,7 @@
 # this file, __init__.py, will be run automatically when someone
 # imports mypackage
 
-from .mypackage import a
-from .mypackage import b
+from . import a
+from . import b
 
 print('Hello from mypackage!')
