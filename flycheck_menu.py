23538e5bd0bdf0693d66673f0b80d3a771c8795b
Reuven Lerner <reuven@lerner.co.il>
2022-05-29 10:18:21 +0300

    gitautosync update 56

---
+++ flycheck_menu.py
@@ -0,0 +1,13 @@
+def menu(*args):
+    while True:
+        user_choice = input(f'Choose from: {"/".join(args)}').strip()
+
+        if user_choice in args:
+            return user_choice
+
+        print(f'{user_choice} is not valid; try again')
+
+
+if __name__ == '__main__':
+    print('Example menu!')
+    
